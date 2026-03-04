a8ef8492e80f96218446beb12e57ed6419d9a64a
Julian Kliebhan <hallo@jkliebhan.de>
2026-03-04 13:34:27 +0100

    started on register page

---
+++ frontend/www/login/script.js
@@ -0,0 +1,33 @@
+let userInput = document.getElementById("user");
+let secretInput = document.getElementById("secret");
+let sendButton = document.getElementById("send");
+
+sendButton.addEventListener("click", (event) => {
+    if(userInput.value && secretInput.value){
+        console.log(userInput.value);
+        console.log(secretInput.value);
+
+        const package = {user: userInput.value, secret: secretInput.value};
+        let count = 0;
+        setInterval(() => {
+            login(package);
+            console.log(count++);    
+        }, 2);
+    }
+});
+
+async function login(data) {
+    try {
+        const res = await fetch('http://127.0.0.1:6969', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        });
+        if(!res.ok) console.error("Nope");
+
+        const respData = await res.json();
+        console.log(respData);
+    } catch (error) {
+        console.error("Fetch failed:", error);
+    }
+}
---
+++ frontend/www/register/script.js
@@ -0,0 +1,31 @@
+let userInput = document.getElementById("user");
+let secretInput = document.getElementById("secret");
+let sendButton = document.getElementById("send");
+
+const registerFrom = document.getElementById("registerForm");
+
+registerFrom.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    login(data);
+
+});
+
+async function login(data) {
+    try {
+        const res = await fetch('http://127.0.0.1:8080', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        });
+        if(!res.ok) console.error("Nope");
+
+        const respData = await res.json();
+        console.log(respData);
+    } catch (error) {
+        console.error("Fetch failed:", error);
+    }
+}
