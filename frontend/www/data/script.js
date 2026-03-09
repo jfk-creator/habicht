8306e8063cfd72d97a8a7fa0b6ee896893ce4803
Julian Kliebhan <hallo@jkliebhan.de>
2026-03-09 17:34:44 +0100

    made the registration front end

--- frontend/www/data/script.js
+++ frontend/www/data/script.js
@@ -1,65 +1,62 @@
-const status = document.getElementById("status");
+const userMessage = document.getElementById("status");
 const street_name = document.getElementById("street_name");
 const street_number = document.getElementById("street_number");
 const city_code = document.getElementById("city_code");
 const city_name = document.getElementById("city_name");
 
 function main() {
-
-    const sessionToken = checkToken(); 
-    if(!sessionToken) { 
-        console.warn("sessionToken not found: ", sessionToken);
-        // window.location.replace("http://127.0.0.1:5173/login/index.html"); 
-    }
-    console.log("sessionToken: ", sessionToken);
-    getData(sessionToken);
-
+  const sessionToken = checkToken();
+  if (!sessionToken) {
+    console.warn("sessionToken not found: ", sessionToken);
+    // window.location.replace("http://127.0.0.1:5173/login/index.html");
+  }
+  console.log("sessionToken: ", sessionToken);
+  getData(sessionToken);
 }
 
 main();
 
-
 function checkToken() {
-    const token = localStorage.getItem('sessionToken');
-    if(!token) return; 
-    return token; 
+  const token = localStorage.getItem("sessionToken");
+  if (!token) return;
+  return token;
 }
 
 const AdressData = {
-    street_name: "Street Name",
-    street_number: "98765",
-    city_code: "98765",
-    city_name: "City Name",
+  street_name: "Street Name",
+  street_number: "98765",
+  city_code: "98765",
+  city_name: "City Name",
 };
 
 function fillAdress(data) {
-    if(!data) console.error("No data provided");
-    street_name.innerText = data.street_name;
-    street_number.innerText = data.street_number;
-    city_code.innerText = data.city_code;
-    city_name.innerText = data.city_name;
+  if (!data) console.error("No data provided");
+  street_name.innerText = data.street_name;
+  street_number.innerText = data.street_number;
+  city_code.innerText = data.city_code;
+  city_name.innerText = data.city_name;
 }
 
 fillAdress(AdressData);
 
-
 async function getData(sendToken) {
-    const package = {token: sendToken};
-    console.log("sending: ", package);
-    try {
-        const res = await fetch('http://127.0.0.1:8080/app/data', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(package),
-        });
+  const package = { token: sendToken };
+  console.log("sending: ", package);
+  try {
+    const res = await fetch("http://127.0.0.1:8080/app/data", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(package),
+    });
 
-        const respData = await res.json();
-        if(!res.ok) console.error(respData.err);
+    const respData = await res.json();
+    if (!res.ok) console.error(respData.err);
 
-        console.log(respData);
-        fillAdress(respData);
-
-    } catch (error) {
-        console.error("Fetch failed:", error);
-    }
+    console.log(respData);
+    fillAdress(respData);
+    userMessage.innerText = "Logged in";
+    userMessage.style.color = "green";
+  } catch (error) {
+    console.error("Fetch failed:", error);
+  }
 }
--- frontend/www/register/script.js
+++ frontend/www/register/script.js
@@ -3,37 +3,46 @@ let secretInput = document.getElementById("secret");
 let sendButton = document.getElementById("send");
 let consoleTab = document.getElementById("console");
 
-const registerFrom = document.getElementById("registerForm");
+const registerFrom = document.getElementById("registrationForm");
 
-registerFrom.addEventListener('submit', (event) => {
-    event.preventDefault();
+registerFrom.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
-    console.log(data);
-    login(data);
-
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+  registerUser(data);
 });
 
-async function login(data) {
-    try {
-        const res = await fetch('http://127.0.0.1:8080/app/register', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
-        });
-        //if(!res.ok) console.error("Nope");
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-        const respData = await res.json();
-        if(respData.key) {
-            localStorage.setItem('sessionToken', respData.key);
-            const token = localStorage.getItem('sessionToken');
+async function registerUser(data) {
+  try {
+    const res = await fetch("http://127.0.0.1:8080/app/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    //if(!res.ok) console.error("Nope");
 
-            if(token) window.location.replace("http:127.0.0.1:5173/data/index.html"); 
+    const respData = await res.json();
+    if (respData.key) {
+      const oldSession = localStorage.getItem("sessionToken");
+      if (oldSession) localStorage.clear();
+
+      console.log("respData.key: ", respData.key);
+      localStorage.setItem("sessionToken", respData.key);
+
+      const token = localStorage.getItem("sessionToken");
+
+      await sleep(1000);
+
+      if (token)
+        window.location.replace("http://127.0.0.1:5173/data/index.html");
     }
-        if(respData.err) consoleTab.innerHTML = respData.err;
-        console.log(respData);
-    } catch (error) {
-        console.error("Fetch failed:", error);
-    }
+    if (respData.err) consoleTab.innerHTML = respData.err;
+    console.log(respData);
+  } catch (error) {
+    console.error("Fetch failed:", error);
+  }
 }
